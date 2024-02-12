2ac784042894d163160c6977a50cad829638a97d
Jason Sturges <jason@jasonsturges.com>
2024-02-12 00:32:57 -0600

    Pixi.js scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,41 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { Application, SCALE_MODES, Sprite } from "pixi.js";
+import { Viewport } from "pixi-viewport";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector<HTMLCanvasElement>("canvas");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const app = new Application({
+  view: canvas as HTMLCanvasElement,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  antialias: true,
+  autoDensity: true,
+  backgroundColor: 0x131313,
+  resolution: devicePixelRatio || 1,
+  resizeTo: window,
+});
+
+const viewport = new Viewport({
+  worldWidth: 1000,
+  worldHeight: 1000,
+  screenWidth: window.innerWidth,
+  screenHeight: window.innerHeight,
+  events: app.renderer.events,
+})
+  .drag()
+  .pinch()
+  .wheel()
+  .decelerate();
+
+app.stage.addChild(viewport);
+
+const bunny = Sprite.from("https://pixijs.com/assets/bunny.png");
+bunny.texture.baseTexture.scaleMode = SCALE_MODES.NEAREST;
+bunny.anchor.set(0.5);
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+viewport.addChild(bunny);
+
+app.ticker.add(() => {
+  bunny.rotation += 0.1 * app.ticker.deltaTime;
+});
